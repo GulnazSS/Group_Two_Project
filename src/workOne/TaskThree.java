517b32422d38bba56c1aee26aa848b23a96f9aea
Anna Taylor <evergreen19@yandex.ru>
2021-10-26 23:31:52 -0400

    task three

---
+++ src/workOne/TaskThree.java
@@ -0,0 +1,10 @@
+package workOne;
+
+public class TaskThree {
+
+    //creating a third task for practice
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
